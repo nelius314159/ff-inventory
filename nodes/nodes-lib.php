80bad9bfb545f83621f2794a7032a8394266ff07
nelius314159 <nelius314159@gmail.com>
2022-01-08 13:27:02 +0200

    Testing Solid mysql access.

--- nodes/nodes-lib.php
+++ nodes/nodes-lib.php
@@ -3,13 +3,36 @@
 //$DATA_DIR = '/var/ff-inventory';
 
 
-//-------------------------------------------------------------------------------------------- 
-function get_nodes () {
+define ('DBUSERSLD','bunpeest651a64r');
+define ('DBPASSSLD','33vj543rlcr978');
+define ('DBNAMESLD','frogfootdb');
+
+
+
+    $vhserver_IP = "41.85.15.50:3306";  // jhb-solid-db01
+
+    $vhserver_db_unsername = DBUSERSLD ; 
+    $vhserver_db_password  = DBPASSSLD ; 
+    $vhserver_db_name = DBNAMESLD ; 
+    //$mastertable      = "test1";
     
-    
- 
-    return 0;
-}
+
+    // Connect to DB-server
+    $link = mysql_connect($vhserver_IP, $vhserver_db_unsername, $vhserver_db_password);
+
+    if (!$link) {
+
+	echo "Could not connect to db: SQL Error: " . mysql_error();
+	return 1;
+    }
+    else {
+	echo "Opened\n";
+    }
+
+
+
+
+
 //-------------------------------------------------------------------------------------------- 
 
 
--- nodes/nodes.php
+++ nodes/nodes.php
@@ -3,16 +3,23 @@
 include('nodes-lib.php');
 
 
+// Nodes structure.
+// - The Id is the primary key.
+// - Work wth exact Solid field names and data by default. E.g. node node name ambiguityes. Use Solid exact.
+
 $nodes = array(
-	       "id" => array(
-			     10,
-			     20
-			     ),
-	       "name" => array(
-			       "Caledon",
-			       "Swellendam"
-			       )
+	       "10404" => array(
+			       "Node Name"             => "Caledon Node",
+			       "Node Room GPS"         => "-34.228356,19.423488"
+				),
+	       "10400" => array(
+			       "Node Name"             => "Swellendam Node",
+			       "Node Room GPS"         => "-34.024324,20.444078"
+				)
 	       );
+	       
+	       
+			
 			 
 //-------------------------------------------------------------------------------------------- 
  
@@ -21,6 +28,7 @@ $nodes = array(
 
 echo "Start.\n";
 
+print_r($nodes);
 
 
 
